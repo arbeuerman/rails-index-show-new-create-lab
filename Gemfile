fa6175c1bb7aaa934c005baf3b5f0338084a92cb
Alexandra <arbeuerman@gmail.com>
2021-04-07 10:11:31 -0500

    adding show and index pages

--- Gemfile
+++ Gemfile
@@ -40,3 +40,6 @@ group :development, :test do
   gem 'capybara'
   gem 'database_cleaner'
 end
+
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+20.times do
+    Coupon.create(store: Faker::Company.name, coupon_code: Faker::Code.nric)
+end
